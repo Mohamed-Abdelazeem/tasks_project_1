d9130f86e65326586667a87d65abccda6645f63d
Mohamed Abdelazeem <115993404+Mohamed-Abdelazeem@users.noreply.github.com>
2023-08-31 21:57:20 +0300

    Add files via upload

---
+++ project/mastar.js
@@ -0,0 +1,18 @@
+let tasks = [
+    {
+        "title": "I have a meeting",
+        "date": "25/10/2023",
+        "isDone": false
+    },
+    {
+        "title": "Going to Work",
+        "date": "25/09/2023",
+        "isDone": true
+    },
+    {
+        "title": "Done Received",
+        "date": "31/09/2023",
+        "isDone": true
+    }
+]
+document.getElementById("tasks").innerHTML = "<h1> Hello world </h1>"
